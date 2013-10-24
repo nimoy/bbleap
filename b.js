270e0f313b7d213cf2d4fd7dce9eef3cbc107b59
nimoy <ag@theblacksea.cc>
2013-10-24 11:23:44 -0600

    added modules and roughed in files

---
+++ b.js
@@ -0,0 +1,25 @@
+// CLIENT
+
+var host = window.document.location.host.replace(/:.*/, '')
+var lp = require('./leap.js')
+var wss = require('websocket-stream')
+var ws = wss('ws://'+host)
+
+var container = document.body
+
+var cvsView = document.querySelector('.main')
+
+paper.setup(cvsView)
+
+var leap = new lp()
+
+ws.on('open', function () {
+  console.log('connection active')
+})
+
+ws.pipe(leap).pipe(ws)
+
+ws.on('data', function (d) {
+  var msg = JSON.parse(d)
+  console.log(d)
+})
---
+++ s.js
@@ -0,0 +1,45 @@
+// SERVER
+
+var osc = require('omgosc')
+var ws = require('ws')
+var http = require('http')
+
+var server = http.createServer(function (req,res) {
+  if(req.url === '/') req.url = '/i.html'
+  console.log('.'+req.url)
+  filed('.'+req.url).pipe(res)
+})
+
+server.listen(80, function () {
+  console.log('running')
+})
+
+var w = new ws({server:server})
+
+w.on('connection', function (wsoc) {
+  soc = wss(wsoc)
+  data.pipe(soc)
+  soc.on('data', function (d) {
+    var cmd = JSON.parse(d)
+    data.write(cmd)
+  })
+  soc.on('close', function () {
+    data.end()
+  })
+})
+
+
+var sender = new osc.UdpSender('127.0.0.1', 7777);
+
+var receiver = new osc.UdpReceiver(7777);
+receiver.on('', function(e) {
+  console.log(e);
+});
+
+var i = 0;
+
+setInterval(function() {
+  sender.send('/osc_data',
+              'sfiTFNI',
+              ['hello', Math.random(), i++, true, false, null, undefined]);
+}, 1000/10);
